2d430d02d7e9719cb1234b1b75b8139732c118bf
vega-star <contato.cvianna@gmail.com>
2024-04-10 23:12:02 -0300

    Trying without required_version

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,9 @@
 terraform {
+  # required_version = "1.7.5"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = ">= 5.24.0"
     }
   }
-
-  required_version = "1.7.9"
 }
