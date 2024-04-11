579410477a57737b0538cf010145a6c35f659e5a
vega-star <contato.cvianna@gmail.com>
2024-04-10 23:13:59 -0300

    Patching fmt -check

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 //SETUP
 provider "google" {
-  project     = var.PROJECT_ID
-  region      = var.SELECTED_REGION
+  project = var.PROJECT_ID
+  region  = var.SELECTED_REGION
 }
 
 data "google_service_account" "terraform_service_account" { # The service account must have been created prior to the execution of Terraform, and the permissions needed granted
@@ -10,27 +10,27 @@ data "google_service_account" "terraform_service_account" { # The service accoun
 
 resource "google_project_service" "gcp_services" {
   for_each = toset(var.USED_API_LIST)
-  project = var.PROJECT_ID
-  service = each.key
+  project  = var.PROJECT_ID
+  service  = each.key
 }
 
 // CORE
 module "core" {
   source = "./modules/core"
 
-  PROJECT_ID = var.PROJECT_ID
-  SELECTED_LOCATION = var.SELECTED_LOCATION
-  SELECTED_REGION = var.SELECTED_REGION
-  TF_SERVICE_ACCOUNT_ID = var.TF_SERVICE_ACCOUNT_ID
-  TF_SERVICE_ACCOUNT_EMAIL = data.google_service_account.terraform_service_account.email
-  DB_ROOT_USERNAME = var.DB_ROOT_USERNAME
-  DB_INSTANCE_TIER = var.DB_INSTANCE_TIER
-  DB_PASSWORD = var.DB_PASSWORD
-  DB_HIGH_AVAILABILITY = var.DB_HIGH_AVAILABILITY
-  VPC_NAME = var.VPC_NAME
+  PROJECT_ID                   = var.PROJECT_ID
+  SELECTED_LOCATION            = var.SELECTED_LOCATION
+  SELECTED_REGION              = var.SELECTED_REGION
+  TF_SERVICE_ACCOUNT_ID        = var.TF_SERVICE_ACCOUNT_ID
+  TF_SERVICE_ACCOUNT_EMAIL     = data.google_service_account.terraform_service_account.email
+  DB_ROOT_USERNAME             = var.DB_ROOT_USERNAME
+  DB_INSTANCE_TIER             = var.DB_INSTANCE_TIER
+  DB_PASSWORD                  = var.DB_PASSWORD
+  DB_HIGH_AVAILABILITY         = var.DB_HIGH_AVAILABILITY
+  VPC_NAME                     = var.VPC_NAME
   KUBERNETES_NODE_MACHINE_TYPE = var.KUBERNETES_NODE_MACHINE_TYPE
-  KUBERNETES_NAMESPACE = var.KUBERNETES_NAMESPACE
-  KUBERNETES_APPLICATION_ID = var.KUBERNETES_APPLICATION_ID
-  GCR_BUCKET_NAME = var.GCR_BUCKET_NAME
-  GCR_REPOSITORY_ID = var.GCR_REPOSITORY_ID
+  KUBERNETES_NAMESPACE         = var.KUBERNETES_NAMESPACE
+  KUBERNETES_APPLICATION_ID    = var.KUBERNETES_APPLICATION_ID
+  GCR_BUCKET_NAME              = var.GCR_BUCKET_NAME
+  GCR_REPOSITORY_ID            = var.GCR_REPOSITORY_ID
 }
--- variables.tf
+++ variables.tf
@@ -1,97 +1,97 @@
 // MAIN VARIABLES
 variable "PROJECT_ID" {
-  type = string
+  type        = string
   description = "The ID of the project on which the infrastructure will be located in GCP."
-  default = "ilion-demo"
+  default     = "ilion-demo"
 }
 
 variable "SELECTED_LOCATION" {
-  type = string
+  type        = string
   description = "General location used in some resources. Not necessarily tied to resources in selected region."
-  default = "US"
+  default     = "US"
 }
 
 variable "SELECTED_REGION" {
-  type = string
+  type        = string
   description = "Main region of the project, on which most resources will be located at."
-  default = "us-central1"
+  default     = "us-central1"
 }
 
 variable "TF_SERVICE_ACCOUNT_ID" {
-  type = string
+  type        = string
   description = "ID of the service account used by terraform to deploy resources."
-  sensitive = true
+  sensitive   = true
 }
 
 variable "USED_API_LIST" {
-  type = list(string)
-  description ="The list of apis necessary for the project to function. This prevents errors when planning infrastructure in a new, clean project."
+  type        = list(string)
+  description = "The list of apis necessary for the project to function. This prevents errors when planning infrastructure in a new, clean project."
   default = [
     "compute.googleapis.com",
     "containerregistry.googleapis.com",
-	  "container.googleapis.com"
+    "container.googleapis.com"
   ]
 }
 
 // DATABASE
 variable "DB_INSTANCE_TIER" {
-  type = string
+  type        = string
   description = "Tier/size of the instance running the database. It defaults to a very small one"
-  default = "db-f1-micro"
+  default     = "db-f1-micro"
 }
 
 variable "DB_ROOT_USERNAME" {
-  type = string
+  type        = string
   description = "Optional parameter. Defaults to root"
-  default = "root"
+  default     = "root"
 }
 
 variable "DB_PASSWORD" {
-  type = string
+  type        = string
   description = "Optional parameter. The DB generates a random password stored in .tfstate if not declared"
-  sensitive = true
+  sensitive   = true
 }
 
 variable "DB_HIGH_AVAILABILITY" {
-  type = bool
+  type        = bool
   description = "Optional parameter. Defaults to false."
-  default = false
+  default     = false
 }
 
 // NETWORKING
 variable "VPC_NAME" {
-  type = string
+  type        = string
   description = "Name of the VPC used by the core module"
-  default = "ilion-vpc-network"
+  default     = "ilion-vpc-network"
 }
 
 // KUBERNETES
 variable "KUBERNETES_NODE_MACHINE_TYPE" {
-  type = string
+  type        = string
   description = "The machine type/size for each node in the main cluster"
-  default = "e2-small"
+  default     = "e2-small"
 }
 
 variable "KUBERNETES_NAMESPACE" {
-  type = string
+  type        = string
   description = "Namspace used by the kubernetes cluster configuration"
-  default = "parking-application-tf"
+  default     = "parking-application-tf"
 }
 
 variable "KUBERNETES_APPLICATION_ID" {
-  type = string
+  type        = string
   description = "Name of the application that can be used to identify the service"
-  default = "parking-application"
+  default     = "parking-application"
 }
 
 variable "GCR_BUCKET_NAME" {
-  type = string
+  type        = string
   description = "The name of the bucket on which GCR will store data"
-  default = "ilion-demo-gcr-bucket"
+  default     = "ilion-demo-gcr-bucket"
 }
 
 variable "GCR_REPOSITORY_ID" {
-  type = string
+  type        = string
   description = "The name/ID of the repository for the service on GKE"
-  default = "ilion-repo"
+  default     = "ilion-repo"
 }
